e483f9bb0ec5598d994563db1e0ea0a6b8a6a1a0
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-03-14 09:44:19 +0100

    heure mise a jour auto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 /**
  * Created by Thomas on 13/03/2017.
  */
-$(".widget")
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+function loadDate() {
+    var dayNames = ["Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat."]
+    var newDate = new Date();
+    newDate.setDate(newDate.getDate());
+    $('#date').html(dayNames[newDate.getDay()] + " " + formatAMPM(newDate));
+}
+
+window.setInterval(loadDate,30000);
+$(document).ready(function () {
+    loadDate();
+})
