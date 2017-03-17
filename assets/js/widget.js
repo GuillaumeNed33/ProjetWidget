e8c383f422de3f1e33518bb4eb53402f88b940bd
Guillaume Nedelec <guillaume.nedelec@etu.u-bordeaux.fr>
2017-03-17 11:15:54 +0100

    suppr comment

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -2,99 +2,6 @@
  * Created by Guillaume on 15/03/2017.
  */
 
-/*function setDateAndTime()
- {
- var currentdate = new Date();
- var datetime = currentdate.getDate() + "/";
-
- if((currentdate.getMonth()+1)<10)
- datetime = datetime + "0" + (currentdate.getMonth()+1)  + "/";
-
- else
- datetime = datetime + (currentdate.getMonth()+1) + "/";
-
- datetime = datetime + currentdate.getFullYear();
-
- if(currentdate.getHours()<10)
- var hourtime = "0" + currentdate.getHours() + ":";
-
- else
- var hourtime = currentdate.getHours() + ":";
-
- if(currentdate.getMinutes()<10)
- hourtime = hourtime + "0" + currentdate.getMinutes() + ":";
- else
- hourtime = hourtime + currentdate.getMinutes() +":";
-
- if(currentdate.getSeconds()<10)
- hourtime = hourtime + "0" + currentdate.getSeconds();
-
- else
- hourtime = hourtime + currentdate.getSeconds();
-
- $("b#dateId").html(datetime);
- $("b#hourId").html(hourtime);
- }
-
- function createDateTimeWidget(content)
- {
- var divCard = document.createElement("div");
- var divContentCard = document.createElement("div");
- var icon = document.createElement("i");
- var button = document.createElement("a");
- var title = document.createElement("h4");
- var ligne = document.createElement("br");
- var date = document.createElement("h5");
- var hour = document.createElement("h5");
-
- $(divCard).attr("class","card-panel light-blue darken-4 hoverable");
- //$(divCard).attr("id","dateHourWidget");
- $(divContentCard).attr("class","card-content");
- $(icon).attr("class","material-icons");
- $(button).attr("class","right btn-floating N/A transparent");
- $(button).attr("onClick","deleteWidget('dateHourWidget');")
- $(title).attr("class", "center-align indigo-text text-lighten-5");
- $(date).attr("class","center-align indigo-text text-lighten-5");
- $(hour).attr("class","center-align indigo-text text-lighten-5");
-
- $(icon).html("clear");
- $(button).append($(icon));
- $(title).html("<b>Date et Heure</b>");
- $(date).html("Date : <b id='dateId'></b>");
- $(hour).html("Heure : <b id='hourId'></b>");
-
- $(divContentCard).append($(button));
- $(divContentCard).append($(title));
- $(divContentCard).append($(ligne));
- $(divContentCard).append($(date));
- $(divContentCard).append($(ligne));
- $(divContentCard).append($(hour));
-
- $(divCard).append($(divContentCard));
-
- $(divCard).fadeIn(0);
- content.append(divCard);
- }
-
-
- function openClockWidget2()
- {
- var mainContent = document.getElementById("content");
- var base = document.createElement("div");
- $(base).attr("class", "panel col-md-4");
- var title = document.createElement("div");
- $(title).attr("class", "panel-heading");
- $(title).html("Clock");
- var content = document.createElement("div");
- $(content).attr("id","dateHourWidget");
- $(content).attr("class","panel-body");
- createDateTimeWidget($(content));
- widgetDateHeure=true;
- setInterval("setDateAndTime()",1000);
- $(base).append($(title), $(content));
- $(mainContent).append($(base));
- }*/
-
 /*** CLOCK ***/
 function openClockWidget() {
     $("#content").append(
@@ -193,11 +100,12 @@ function loadMeteo() {
     $.getJSON(url,function(data){
         var icone = data.weather[0].icon;
         var temperature = data.main.temp;
+        var desc = data.weather.main;
         temperature= parseFloat(temperature);
         parseInt(temperature);
         temperature =parseInt(temperature-273.15);
         temperature = temperature.toString();
-        $('#contentMeteo').append("Temps : <img src='http://openweathermap.org/img/w/"+ icone + ".png' /><br/><br/>");
+        $('#contentMeteo').append("Temps : <img src='http://openweathermap.org/img/w/"+ icone + ".png' /><br/><br/>" + desc);
         $('#contentMeteo').append('Température : ' + temperature + '°C<br>');
     })
 }
