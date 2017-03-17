176f9cebbe0d2e48babaf8ba463466864cd253cd
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-03-17 11:14:29 +0100

    Ajout bouton internet explorer + début script useless web

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -1,100 +1,6 @@
 /**
  * Created by Guillaume on 15/03/2017.
  */
-
-/*function setDateAndTime()
- {
- var currentdate = new Date();
- var datetime = currentdate.getDate() + "/";
-
- if((currentdate.getMonth()+1)<10)
- datetime = datetime + "0" + (currentdate.getMonth()+1)  + "/";
-
- else
- datetime = datetime + (currentdate.getMonth()+1) + "/";
-
- datetime = datetime + currentdate.getFullYear();
-
- if(currentdate.getHours()<10)
- var hourtime = "0" + currentdate.getHours() + ":";
-
- else
- var hourtime = currentdate.getHours() + ":";
-
- if(currentdate.getMinutes()<10)
- hourtime = hourtime + "0" + currentdate.getMinutes() + ":";
- else
- hourtime = hourtime + currentdate.getMinutes() +":";
-
- if(currentdate.getSeconds()<10)
- hourtime = hourtime + "0" + currentdate.getSeconds();
-
- else
- hourtime = hourtime + currentdate.getSeconds();
-
- $("b#dateId").html(datetime);
- $("b#hourId").html(hourtime);
- }
-
- function createDateTimeWidget(content)
- {
- var divCard = document.createElement("div");
- var divContentCard = document.createElement("div");
- var icon = document.createElement("i");
- var button = document.createElement("a");
- var title = document.createElement("h4");
- var ligne = document.createElement("br");
- var date = document.createElement("h5");
- var hour = document.createElement("h5");
-
- $(divCard).attr("class","card-panel light-blue darken-4 hoverable");
- //$(divCard).attr("id","dateHourWidget");
- $(divContentCard).attr("class","card-content");
- $(icon).attr("class","material-icons");
- $(button).attr("class","right btn-floating N/A transparent");
- $(button).attr("onClick","deleteWidget('dateHourWidget');")
- $(title).attr("class", "center-align indigo-text text-lighten-5");
- $(date).attr("class","center-align indigo-text text-lighten-5");
- $(hour).attr("class","center-align indigo-text text-lighten-5");
-
- $(icon).html("clear");
- $(button).append($(icon));
- $(title).html("<b>Date et Heure</b>");
- $(date).html("Date : <b id='dateId'></b>");
- $(hour).html("Heure : <b id='hourId'></b>");
-
- $(divContentCard).append($(button));
- $(divContentCard).append($(title));
- $(divContentCard).append($(ligne));
- $(divContentCard).append($(date));
- $(divContentCard).append($(ligne));
- $(divContentCard).append($(hour));
-
- $(divCard).append($(divContentCard));
-
- $(divCard).fadeIn(0);
- content.append(divCard);
- }
-
-
- function openClockWidget2()
- {
- var mainContent = document.getElementById("content");
- var base = document.createElement("div");
- $(base).attr("class", "panel col-md-4");
- var title = document.createElement("div");
- $(title).attr("class", "panel-heading");
- $(title).html("Clock");
- var content = document.createElement("div");
- $(content).attr("id","dateHourWidget");
- $(content).attr("class","panel-body");
- createDateTimeWidget($(content));
- widgetDateHeure=true;
- setInterval("setDateAndTime()",1000);
- $(base).append($(title), $(content));
- $(mainContent).append($(base));
- }*/
-
 /*** CLOCK ***/
 function openClockWidget() {
     $("#content").append(
@@ -174,12 +80,12 @@ function openMeteoWidget() {
         '<div class="panel panel-default" style="margin-left: 500px;">'+
         '<div class="panel-heading"><img src="assets/img/meteo.png" style="width: 20px; margin-right: 5px; margin-top: 0;">Meteo</div>' +
         '<div class="panel-body">' +
-        '<div id="formMeteo" class="input-group" style="text-align:center;">'+
+        '<form id="formMeteo" class="input-group" style="text-align:center;">'+
         '<input type="text" class="form-control" id="city" placeholder="Entrer une Ville">' +
         '<div class="input-group-addon">' +
         '<button class="glyphicon glyphicon-search" type="submit" onclick="loadMeteo()" style="background-color: transparent; border: 0;"></button>'+
         '</div>' +
-        '</div>'+
+        '</form>'+
         '<div id="contentMeteo"></div>' +
         '</div>'+
         '</div>');
@@ -212,12 +118,12 @@ function openPhotoWidget() {
         '<div class="panel panel-default" style="margin-left: 500px; width: 400px;">'+
         '<div class="panel-heading"><img src="assets/img/photo.png" style="width: 20px; margin-right: 5px; margin-top: 0;">Photos</div>' +
         '<div class="panel-body">' +
-        '<div id="formPhotos" class="input-group" style="text-align:center;">'+
+        '<form id="formPhotos" class="input-group" style="text-align:center;">'+
         '<input type="text" class="form-control" id="searchPhoto" placeholder="Recherche de Photos">' +
         '<div class="input-group-addon">' +
         '<button class="glyphicon glyphicon-search" type="submit" onclick="loadPhoto()" style="background-color: transparent; border: 0;"></button>'+
         '</div>' +
-        '</div>'+
+        '</form>'+
         '<div id="contentPhoto"></div>' +
         '</div>'+
         '</div>');
@@ -228,7 +134,13 @@ function openPhotoWidget() {
 
 /*** TWITTER ***/
 function openTwitterWidget() {
-
+    $("#content").append(
+        '<div class="panel panel-default" style="margin-left: 500px; width: 400px;">'+
+        '<div class="panel-heading"><img src="assets/img/twitter.png" style="width: 20px; margin-right: 5px; margin-top: 0;">Twitter</div>' +
+        '<div class="panel-body">' +
+        '<div id="contentTwitter"><a class="twitter-timeline" data-lang="fr" data-width="400" data-height="400" data-theme="light" data-link-color="#2B7BB9" href="https://twitter.com/MichelBillaud">Tweets by MichelBillaud</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></div>' +
+        '</div>'+
+        '</div>');
 }
 
 
@@ -239,12 +151,12 @@ function openYoutubeWidget() {
         '<div class="panel panel-default" style="margin-left: 500px; width: 400px;">'+
         '<div class="panel-heading"><img src="assets/img/youtube.png" style="width: 20px; margin-right: 5px; margin-top: 0;">Youtube</div>' +
         '<div class="panel-body">' +
-        '<div id="formYoutube" class="input-group" style="text-align:center;">'+
+        '<form id="formYoutube" class="input-group" style="text-align:center;">'+
         '<input type="text" class="form-control" id="searchYoutube" placeholder="Entrer des mots clefs">' +
         '<div class="input-group-addon">' +
         '<button class="glyphicon glyphicon-search" type="submit" onclick="loadYoutube()" style="background-color: transparent; border: 0;"></button>'+
         '</div>' +
-        '</div>'+
+        '</form>'+
         '<div id="contentYoutube"></div>' +
         '</div>'+
         '</div>');
@@ -265,12 +177,12 @@ function openMapWidget() {
         '<div class="panel panel-default" style="margin-left: 500px; width: 400px;">'+
         '<div class="panel-heading"><img src="assets/img/map.png" style="width: 20px; margin-right: 5px; margin-top: 0;">Google Maps</div>' +
         '<div class="panel-body">' +
-        '<div id="formMap" class="input-group" style="text-align:center;">'+
+        '<form id="formMap" class="input-group" style="text-align:center;">'+
         '<input type="text" class="form-control" id="cityMap" placeholder="Choisir la ville de départ vers l\'IUT">' +
         '<div class="input-group-addon">' +
         '<button class="glyphicon glyphicon-search" type="submit" onclick="loadMap()" style="background-color: transparent; border: 0;"></button>'+
         '</div>' +
-        '</div>'+
+        '</form>'+
         '<div id="contentMap"></div>' +
         '</div>'+
         '</div>');
